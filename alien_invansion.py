0bae65894dc84f388a2c64a76f980f62fd91c75f
Natneam <se.natneam.mesele@gmail.com>
2020-08-01 18:04:43 +0300

    writting the main game loop in file alien invasion.py  and settings of the game  setting.py

---
+++ alien_invansion.py
@@ -0,0 +1,42 @@
+'''
+Main file of Alien Invension game
+
+In this game( Alien Invasion), the player controls a ship that appears at
+the bottom center of the screen. The player can move the ship
+right and left using the arrow keys and shoot bullets using the
+spacebar. When the game begins, a fleet of aliens fills the sky
+and moves across and down the screen. The player shoots and
+destroys the aliens. If the player shoots all the aliens, a new fleet
+appears that moves faster than the previous fleet. If any alien hits
+the player’s ship or reaches the bottom of the screen, the player
+loses a ship. If the player loses three ships, the game ends.
+
+'''
+
+import sys
+import pygame
+from settings import Setting
+
+def run_game():
+    # Initialize pygame, settings and screen object. 
+    pygame.init()
+    game_setting = Setting()
+    screen = pygame.display.set_mode((game_setting.screen_width,game_setting.screen_height))
+
+    pygame.display.set_caption("Alien Invansion")
+
+    # Start the main loop for the game.
+    while True:
+
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Redraw the screen during each pass through the loop.  
+        screen.fill(game_setting.bg_color)
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Setting():
+    """A class to store all settings for Alien Invasion."""
+    def __init__(self):
+        '''Initializing the game setting.'''
+
+        #screen settings
+        self.screen_width = 800
+        self.screen_height = 500
+        self.bg_color = (230,230,230)
+        
